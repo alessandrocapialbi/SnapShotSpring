9e2a2cc3ddea40c1e5cc211e059032c9727484ea
alessandrocapialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-03-12 12:01:19 +0100

    Initial commit

---
+++ src/main/java/com/SWE_photoshoot_booking/SwePhotoshootBookingApplication.java
@@ -0,0 +1,13 @@
+package com.SWE_photoshoot_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwePhotoshootBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwePhotoshootBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SWE_photoshoot_booking/SwePhotoshootBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SWE_photoshoot_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwePhotoshootBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
