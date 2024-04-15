e0a0fd34b579d6a26955de97ee71122f1a83cca1
alessandrocapialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-04-15 17:31:58 +0200

    Implement service layer for Appointment

--- src/main/java/com/SWE_photoshoot_booking/controllers/impl/AppointmentController.java
+++ src/main/java/com/SWE_photoshoot_booking/controllers/impl/AppointmentController.java
@@ -5,7 +5,6 @@ import com.SWE_photoshoot_booking.domain.dto.AppointmentDto;
 import com.SWE_photoshoot_booking.domain.entities.AppointmentEntity;
 import com.SWE_photoshoot_booking.mappers.Mapper;
 import com.SWE_photoshoot_booking.repositories.AppointmentRepository;
-import com.SWE_photoshoot_booking.services.AbstractCrudService;
 import com.SWE_photoshoot_booking.services.impl.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
