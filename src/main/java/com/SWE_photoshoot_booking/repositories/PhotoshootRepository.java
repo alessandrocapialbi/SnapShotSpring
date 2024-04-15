10e3eebf138ed77494b22479db7ba5462e98373f
alessandrocapialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-04-15 15:46:34 +0200

    Implement service layer for Photoshoot

--- src/main/java/com/SWE_photoshoot_booking/repositories/PhotoshootRepository.java
+++ src/main/java/com/SWE_photoshoot_booking/repositories/PhotoshootRepository.java
@@ -2,7 +2,6 @@ package com.SWE_photoshoot_booking.repositories;
 
 import com.SWE_photoshoot_booking.domain.entities.PhotoshootEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
