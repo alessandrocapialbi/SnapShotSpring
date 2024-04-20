83c818f76a6349aafb24c10f989aa29dec9caa36
alessandrocapialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-04-20 12:43:51 +0200

    Implement photographer dashboard

--- src/main/java/com/SWE_photoshoot_booking/controllers/DashboardController.java
+++ src/main/java/com/SWE_photoshoot_booking/controllers/DashboardController.java
@@ -1,10 +1,21 @@
 package com.SWE_photoshoot_booking.controllers;
 
+import org.springframework.ui.Model;
+import com.SWE_photoshoot_booking.domain.entities.UserEntity;
+import com.SWE_photoshoot_booking.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class DashboardController {
+
+    private final UserRepository userRepository;
+
+    public DashboardController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/customer-dashboard")
     public String showUserDashboard() {
@@ -12,7 +23,9 @@ public class DashboardController {
     }
 
     @GetMapping("/photographer-dashboard")
-    public String showPhotographerDashboard() {
+    public String showPhotographerDashboard(Model model, Principal principal) {
+        UserEntity photographer = userRepository.findByEmail(principal.getName());
+        model.addAttribute("photographerName", photographer.getName());
         return "photographer-dashboard";
     }
 }
