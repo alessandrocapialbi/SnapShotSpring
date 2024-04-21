dcf8f6efa52b2df9e1c59239f05a24b1159edd36
alessandrocapialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2024-04-21 22:59:57 +0200

    Implement Update in Photoshoots Management

--- src/main/java/com/SWE_photoshoot_booking/services/impl/PhotoshootService.java
+++ src/main/java/com/SWE_photoshoot_booking/services/impl/PhotoshootService.java
@@ -3,6 +3,8 @@ package com.SWE_photoshoot_booking.services.impl;
 import com.SWE_photoshoot_booking.domain.entities.PhotoshootEntity;
 import com.SWE_photoshoot_booking.repositories.PhotoshootRepository;
 import com.SWE_photoshoot_booking.services.AbstractCrudService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import java.util.UUID;
 @Service
 public class PhotoshootService extends AbstractCrudService<PhotoshootEntity, PhotoshootRepository> {
 
+    private static final Logger logger = LoggerFactory.getLogger(PhotoshootService.class);
+
     @Autowired
     public PhotoshootService(PhotoshootRepository photoshootRepository) {
         super(photoshootRepository);
@@ -20,6 +24,7 @@ public class PhotoshootService extends AbstractCrudService<PhotoshootEntity, Pho
     @Override
     public PhotoshootEntity partialUpdate(UUID id, PhotoshootEntity entity) {
         entity.setPhotoshootID(id);
+        logger.info("Updating photoshoot with id: {}", id);
         return getRepository().findById(id).map(existingPhotoshoot -> {
             Optional.ofNullable(entity.getName()).ifPresent(existingPhotoshoot::setName);
             Optional.ofNullable(entity.getPrice()).ifPresent(existingPhotoshoot::setPrice);
